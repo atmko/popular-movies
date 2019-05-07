ec5c89de1c9cfe0f6521f3987e901acd22e77f04
sigmko <44901543+sigmko@users.noreply.github.com>
2019-05-06 21:56:07 -0400

    create toolbar layout file:     consolidate app toolbar into single layout file and reference     using "include" xml tag
    add toolbar: add toolbar to ReviewActivity

--- app/src/main/java/com/upkipp/popularmovies/activities/ReviewActivity.java
+++ app/src/main/java/com/upkipp/popularmovies/activities/ReviewActivity.java
@@ -6,8 +6,10 @@ package com.upkipp.popularmovies.activities;
 
 import android.content.Intent;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 import com.upkipp.popularmovies.R;
@@ -21,6 +23,18 @@ public class ReviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
+        //toolbar customization
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        //set action bar up(back) button
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+        }
 
         //get retrieved intent
         Intent intent = getIntent();
